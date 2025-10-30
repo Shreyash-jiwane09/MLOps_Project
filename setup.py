c0a219bab97a60076647a2b70f608188d8a7afa6
Shreyash-jiwane09 <shrey.jiwane09@gmail.com>
2025-10-30 13:57:03 +0530

    project structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shreyash Jiwane",
+    author_email="shrey.jiwane09@gmail.com",
+    packages=find_packages()
+)
